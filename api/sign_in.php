7ccaa67c9e75e189713ce6c031a8f844eb12f490
mongeo <gmontague0205@gmail.com>
2021-09-19 23:47:00 -0700

    gandalf

--- api/sign_in.php
+++ api/sign_in.php
@@ -29,8 +29,8 @@ if (isset($_SESSION['username'])) {
         <div class="form-signin">
             <h1 class='title'>Lord of the<br> Pick 'em</h1>
             <form method="POST" action="sign_in_process.php">
-              <img class="mb-4" src="../img/football-icon1.svg" alt="" width="72" height="72">
-              <h1 class="h3 mb-3 font-weight-normal">Please sign in:</h1>
+              <img class="mb-4" src="../img/gandalf.png" alt="" height="200">
+              <h4 class="h4 mb-3 font-weight-normal">Please sign in:</h4>
               <label for="username" class="sr-only">Username</label>
               <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
               <label for="inputPassword" class="sr-only">Password</label>
