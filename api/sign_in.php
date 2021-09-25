a913e2648a67ed7cba180ad2b96eefcfe1a9416e
mongeo <gmontague0205@gmail.com>
2021-09-25 16:38:37 -0700

    signin responsiveness

--- api/sign_in.php
+++ api/sign_in.php
@@ -9,6 +9,7 @@ if (isset($_SESSION['username'])) {
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
