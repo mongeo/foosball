45746c8a8fcc8d59ca54013a9a071ba92d389d15
mongeo <gmontague0205@gmail.com>
2021-10-10 14:24:17 -0700

    updated head2head api logic for timing

--- api/head2head_by_week.php
+++ api/head2head_by_week.php
@@ -7,13 +7,13 @@ $conn = getDatabaseConnection("nfl_picks");
 $arr = array();
 
 $sql1 = "SELECT game_key,
-	home_team,
+home_team,
     away_team,
     pick,
     date_time,
     count(pick) as count_num
 FROM `user_games`
-WHERE date_time >= NOW() + INTERVAL 3 HOUR AND date_time <= NOW() + INTERVAL 7 HOUR
+WHERE NOW() + INTERVAL 3 HOUR >= date_time AND NOW() + INTERVAL 3 HOUR <= date_time + INTERVAL 4 HOUR
 GROUP BY game_key, pick;";
 $stmt1 = $conn->prepare($sql1);
 $stmt1->execute();
